24470f7361b5d97f89e608f52c59b6a3b503a348
Matheus Palhares <79490834+Palhares17@users.noreply.github.com>
2022-02-24 00:49:32 -0300

    design finalizado - falta configurar forms

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
+// menu mobile
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu() {
+	const nav = document.getElementById("nav");
+	nav.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+
+//menu active
+const links = document.querySelectorAll("#menu li a");
+
+function ativarLink(link) {
+	const url = location.href;
+	const href = link.href;
+	if (url.includes(href)) {
+		link.classList.add("active");
+	}
+}
+
+links.forEach(ativarLink);
 // animation
 if (window.SimpleAnime) {
-    new SimpleAnime();
-  }
-  
+	new SimpleAnime();
+}
